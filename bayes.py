6451118e3cf2b91cc90b0ac20c3b9e1b7bf0094c
e-dzia <theladyofthegallifrey@gmail.com>
2019-03-09 23:10:42 +0100

    added evaluation

--- bayes.py
+++ bayes.py
@@ -6,27 +6,32 @@ class Bayes:
     def fit(self, X, y, sample_weight=None):
         # Fit the model according to the given training data.
         # TODO
+        print('fitting')
         return self
 
     def predict(self, X):  # test
         # Perform classification on samples in X (test)
         # TODO
-        y_pred = []
+        print('predicting')
+        y_pred = len(X) * ['some_label']
         return y_pred  # class labels for samples in X
 
     def score(self, X, y, sample_weight=None):
         # Returns the mean accuracy on the given test data and labels.
         # TODO
+        print('scoring')
         score = 0
         return score
 
     def get_params(self, deep=None):
         # Get parameters for this estimator.
         # TODO
+        print('getting params')
         params = {}
         return params
 
     def set_params(self,**params):
         # Set the parameters of this estimator.
         # TODO
+        print('setting params')
         return self
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn import naive_bayes
+from sklearn import naive_bayes, model_selection, metrics
 from bayes import Bayes
 
 
@@ -28,12 +28,16 @@ def unpack_data(filename):
 
 
 def preprocess_data(dataset):
+    # TODO: digitize etc
     return dataset
 
 
 def split_data(dataset):
-    # TODO -> it's not cross-validation yet, tmp: test_size=0.2
+    # split into train and test sets
+    # TODO: stratify?
     train_set, test_set = train_test_split(dataset, test_size=0.2, random_state=42, stratify=dataset['class'])
+
+    # extract labels
     train_set_labels = train_set["class"].copy()
     train_set = train_set.drop("class", axis=1)
 
@@ -43,36 +47,34 @@ def split_data(dataset):
     return train_set, train_set_labels, test_set, test_set_labels
 
 
-def cross_validation(dataset):
-    # for i in range(1):
-    #     train_set, train_set_labels, test_set, test_set_labels = split_data(dataset)
-    #     bayes(train_set)
-    #     evaluate(test_set)
-    train_set, train_set_labels, test_set, test_set_labels = split_data(dataset)
+def cross_validation(dataset, labels, model):
+    seed = 7
+    X = dataset
+    Y = labels
 
-    clf = naive_bayes.MultinomialNB()  # Bayes()
-    clf.fit(train_set, train_set_labels)
-    labels = clf.predict(test_set)
+    kfold = model_selection.KFold(n_splits=10, random_state=seed)
+    cv_results = model_selection.cross_val_score(model, X, Y, cv=kfold, scoring='f1_weighted')
+    # TODO: accuracy? f1_weighted?
+
+    print("Cross-validation mean score: {} ({})".format(cv_results.mean(), cv_results.std()))
+
+
+def evaluate(labels_true, labels_predicted):
+    accuracy = metrics.accuracy_score(y_true=labels_true, y_pred=labels_predicted)
+    f1 = metrics.f1_score(y_true=labels_true, y_pred=labels_predicted, average='weighted')
 
     count = 0
-    for label_bayes, label_true in zip(labels, test_set_labels):
-        print(label_bayes, label_true)
+    for label_bayes, label_true in zip(labels_predicted, labels_true):
+        # print(label_bayes, label_true)
         if label_bayes == label_true:
             count += 1
-    print(len(labels), count)
-
-
-def bayes(dataset):
-    pass
-
-
-def evaluate(dataset):
-    pass
+    print("Correct labels: {}/{}".format(count, len(labels_predicted)))
+    print("Accuracy: {}, F1: {}".format(accuracy, f1))
 
 
 def show_data(dataset):
     print(dataset.head(), "\n")
-    print(dataset.corr())
+    print(dataset.corr(), "\n")
 
     # dataset.hist(bins=50, figsize=(20, 15))
     # plt.show()
@@ -90,7 +92,19 @@ def main(filename, show_mode):
         show_data(dataset)
 
     dataset = preprocess_data(dataset)
-    cross_validation(dataset)
+
+    # TODO: my own Bayes algorithm model
+    model = naive_bayes.GaussianNB()
+
+    # TODO: split the dataset before cross-validation?
+    train_set, train_set_labels, test_set, test_set_labels = split_data(dataset)
+
+    cross_validation(train_set, train_set_labels, model)
+
+    model.fit(train_set, train_set_labels)
+    labels = model.predict(test_set)
+
+    evaluate(test_set_labels, labels)
 
 
 if __name__ == "__main__":
